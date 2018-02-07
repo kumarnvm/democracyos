794b42ca82158ddee66004f8c89afa7a6bf32bd0
florlafuente <lafuente.florencia@gmail.com>
2018-02-07 16:21:01 -0300

    Adds globalize webpack plugin

--- cms/components/admin.js
+++ cms/components/admin.js
@@ -9,7 +9,7 @@ import Navbar from './navbar'
 import { PostList, PostCreate, PostEdit } from './posts'
 
 export default () => (
-  <Admin menu={Navbar} title='Democracy OS' restClient={RestClient} customRoutes={CustomRoutes} history={history} >
+  <Admin menu={Navbar} title='Democracy OS' restClient={RestClient} customRoutes={CustomRoutes} history={history} locale={"es"} >
     <Resource name='posts' list={PostList} create={PostCreate} edit={PostEdit} remove={Delete} />
     <Resource name='settings' list={PostList} />
     <Resource name='reaction-rule' list={PostList} />
